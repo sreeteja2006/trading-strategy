b4ce99bb5627aff0351f8b61ab157f66b711c837
sreeteja <gsreeteja25@gmail.com>
2025-06-17 18:12:02 +0530

    fetching the data form yfinance

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,2 +1,6 @@
 import yfinance as yf
-def extract
+def extract_data(ticker,start_date,end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    data = data[['Open', 'High', 'Low', 'Close', 'Volume']]
+    data.dropna(inplace=True)
+    return data
